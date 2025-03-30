b5f7323ab9c5446dd3d4a845dfa760c29ab6efb4
Isra Teneda <israteneda@gmail.com>
2025-03-29 23:55:15 -0500

    Refactor Tailwind configuration to use ES module syntax and improve export structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import { heroui } from "@heroui/theme";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -18,3 +18,5 @@ module.exports = {
   },
   plugins: [heroui()],
 };
+
+export default config;
